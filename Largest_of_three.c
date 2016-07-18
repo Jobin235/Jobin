95b5628c14dbd23117543eeaebc2407ce9e83544
Jobin235 <jobinjoy235@gmail.com>
2016-07-18 10:41:34 +0530

    Create Largest_of_three.c

---
+++ Largest_of_three.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    float a, b, c;
+    printf("Enter three numbers: ");
+    scanf("%f %f %f", &a, &b, &c);
+    if( a>=b && a>=c )
+        printf("%f is the largest number", a);
+    if( b>=a && b>=c )
+        printf("%f is the largest number", b);
+    if( c>=a && c>=b )
+        printf("%f is the largest number", c);
+    return 0;
+}
